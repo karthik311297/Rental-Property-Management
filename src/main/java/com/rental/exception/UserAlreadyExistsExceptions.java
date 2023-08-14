987dec1da1776251729390ad7a09edb981c7e3b4
Karthik Iyer <karthik.iyer97@gmail.com>
2023-08-14 20:00:59 +0530

    Add few exception classes

---
+++ src/main/java/com/rental/exception/UserAlreadyExistsExceptions.java
@@ -0,0 +1,9 @@
+package com.rental.exception;
+
+public class UserAlreadyExistsExceptions extends Exception
+{
+    public UserAlreadyExistsExceptions(String message)
+    {
+        super(message);
+    }
+}
