60450a67fac0214f663868ff64deb8aa8ecac05f
Karthik Iyer <karthik.iyer97@gmail.com>
2023-08-14 20:06:25 +0530

    Move class to appropriate package

--- src/main/java/com/rental/service/AuthTokenService.java
+++ src/main/java/com/rental/service/AuthTokenService.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.rental.exception.AuthenticatedSessionAlreadyExistsException;
 import com.rental.model.AuthToken;
 import com.rental.model.UserInfo;
 import com.rental.repository.AuthTokenRepository;
